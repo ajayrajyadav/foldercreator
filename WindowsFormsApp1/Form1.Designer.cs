2af8098219367f618b4e9258ca5b10fbc20979a3
ajayrajyadav <ajayraj98@gmail.com>
2018-10-25 22:34:01 -0700

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.btn_fileDialog = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_create_folders = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(342, 328);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(172, 43);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "ok";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            // 
+            // btn_fileDialog
+            // 
+            this.btn_fileDialog.Location = new System.Drawing.Point(571, 88);
+            this.btn_fileDialog.Name = "btn_fileDialog";
+            this.btn_fileDialog.Size = new System.Drawing.Size(62, 62);
+            this.btn_fileDialog.TabIndex = 1;
+            this.btn_fileDialog.Text = "...";
+            this.btn_fileDialog.UseVisualStyleBackColor = true;
+            this.btn_fileDialog.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(497, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(135, 328);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(159, 42);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 124);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(497, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btn_create_folders
+            // 
+            this.btn_create_folders.Location = new System.Drawing.Point(518, 172);
+            this.btn_create_folders.Name = "btn_create_folders";
+            this.btn_create_folders.Size = new System.Drawing.Size(114, 59);
+            this.btn_create_folders.TabIndex = 6;
+            this.btn_create_folders.Text = "button1";
+            this.btn_create_folders.UseVisualStyleBackColor = true;
+            this.btn_create_folders.Click += new System.EventHandler(this.btn_create_folders_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_create_folders);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_fileDialog);
+            this.Controls.Add(this.btn_ok);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Button btn_fileDialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_create_folders;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (FileDialog fileDialog = new OpenFileDialog())
+            {
+                if (DialogResult.OK == fileDialog.ShowDialog())
+                {
+                    string filename = fileDialog.FileName;
+
+                    string directoryPath = Path.GetDirectoryName(filename);
+
+                    textBox1.Text = fileDialog.FileName;
+                    textBox2.Text = directoryPath;
+                }
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("we are exiting!!");
+            Application.Exit();
+        }
+
+        private void btn_create_folders_Click(object sender, EventArgs e)
+        {
+            //todo
+
+            //find all the files and add them to an array
+            string[] array1 = Directory.GetFiles(textBox2.Text);
+            //find all the created dates and add them into the array
+
+            //create  folders for above array
+
+            //create arrays for each folder
+
+            //fill it up wiht array
+
+            //add them to the folders
+        }
+    }
+}
